52db7d130e726fa1e0f400cdb0ee71fb58cf0d6b
Evgenii Dolgov <river100@mail.ru>
2020-08-09 17:05:26 +0300

    Добавлены источники сущностей

---
+++ src/main/java/amaralus/apps/entitymanipulator/MapEntitySource.java
@@ -0,0 +1,16 @@
+package amaralus.apps.entitymanipulator;
+
+import java.util.Map;
+
+public class MapEntitySource extends EntitiesSource<Map.Entry<Object, Object>> {
+
+    public MapEntitySource(Map<Object, Object> map) {
+        super(map::entrySet, Map.Entry::getKey);
+    }
+
+    @Override
+    public Object get(Object object) {
+        var entry = (Map.Entry<?, ?>) super.get(object);
+        return entry == null ? null : entry.getValue();
+    }
+}
